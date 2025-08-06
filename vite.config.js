b08b2b6ab1b8de06c619338ee5c4ba6e176d5f05
Melo <105757660+MeloCoderYT@users.noreply.github.com>
2025-08-05 21:17:47 -0300

    OPs, Forgot to build in

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/AboutMe/', // ← nome do seu repositório aqui!
+  build: {
+    outDir: 'docs',        // ← GitHub Pages só aceita root ou docs/
+  },
+});
